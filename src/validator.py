3bccb3aaa88f590a26faad7d700e2c8bde23903b
Chunde <godadada@users.noreply.github.com>
2017-09-14 19:48:47 -0400

    Update validator.py
    Added JSon data tree traversing routine.

--- src/validator.py
+++ src/validator.py
@@ -2,5 +2,21 @@
 import json
 
 with open('profile.json') as json_data:
-    d = json.load(json_data)
-    print(d)
+    data = json.load(json_data)
+    print(data)
+
+
+#recursive iterator of json data tree
+def recursive_iter(obj):
+    if isinstance(obj, dict):
+        for item in obj.values():
+            yield from recursive_iter(item)
+    elif any(isinstance(obj, t) for t in (list, tuple)):
+        for item in obj:
+            yield from recursive_iter(item)
+    else:
+        yield obj
+
+
+for item in recursive_iter(data):
+    print(item)
