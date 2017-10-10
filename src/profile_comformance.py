30846479de6c41e1ea7cbe54a4ad741947ddc786
Chunde <godadada@users.noreply.github.com>
2017-10-10 06:55:07 -0400

    Update profile_comformance.py

--- src/profile_comformance.py
+++ src/profile_comformance.py
@@ -4,62 +4,95 @@ import sys
 
 
 #compare profile against cert leaf
-def compare_leaf(profile_leaf, cert_leaf):
+def compare_leaf(obj, cert):
+    profile_leaf = obj['Leaf']
+    result = conformance_check_functions[profile_leaf['function']](cert, profile_leaf)
+
+    """
     if profile_leaf['Type'] == "OID":
-          cert_value = cert_leaf[profile_leaf.Name]
     elif profile_leaf['Type'] == "Binary":
-          contents = cert_leaf[profile_leaf.Name].contents
-          cert_value = '{}'.format('%02X' % c for c in contents) 
+          cert_value = '{}'.format('%02X' % c for c in cert_value) 
     elif profile_leaf['Type'] == "String":
-      	  cert_value = cert_leaf[profile_leaf.Name].native
+      	  cert_value = cert[profile_leaf.Name].native
     elif profile_leaf['Type'] == "DateTime":
-      	  cert_value = cert_leaf[profile_leaf.Name]
+      	  cert_value = cert[profile_leaf.Name]
     elif profile_leaf['Type'] == "List":
-      	  cert_value = cert_leaf[profile_leaf.Name]
     elif profile_leaf['Type'] == "Bool":
-      	  cert_value = cert_leaf[profile_leaf.Name]
+      	  cert_value = cert[profile_leaf.Name]
     elif profile_leaf['Type'] == "Tuple":
-      	  cert_value = cert_leaf[profile_leaf.Name]
+      	  cert_value = cert[profile_leaf.Name]
     elif profile_leaf['Type'] == "OrderedDict":
-      	  cert_value = cert_leaf[profile_leaf.Name]
+      	  cert_value = cert[profile_leaf.Name]
     else:
           print("\"WARNING: Unknow Leaf Type\"")
           return
+   
 
-    if profile_leaf['Require'] == "0" and not (profile_leaf['Vaue'] == cert_value):
-      print("\"WARNING: optional value is not valid\"", end="")
-    elif profile_leaf['Require'] == "1" and profile_leaf['Vaue'] == cert_value:
-      print("\"ERROR: value is not allowed\"", end="")
-    elif profile_leaf['Require'] == "2" and not(profile_leaf['Vaue'] == cert_value):
-      print("\"ERROR: required value is not valid\"", end="")
+    if profile_leaf['Require'] == "0" and not (profile_leaf['Value'] == cert_value):
+      result = "WARNING: optional value is not valid"
+    elif profile_leaf['Require'] == "1" and profile_leaf['Value'] == cert_value:
+      result = "ERROR: value is not allowed - " + cert_value 
+    elif profile_leaf['Require'] == "2" and not(profile_leaf['Value'] == cert_value):
+      result = "ERROR: required value is not valid - "  + cert_value
+    elif profile_leaf['Require'] == "PROFILE_IN_CERT" and not( set(profile_leaf['Value']) <= set(cert_value) ):
+      result = "Failed with cert value: " + cert_value
+    elif profile_leaf['Require'] == "CERT_IN_PROFILE" and not(set(cert_value) <=set(profile_leaf['Value'])):
+      result = "Failed with cert value: " + cert_value 
     else:
-      print('"PASS"', end="")
+      result = "PASS"
+     """
+    del obj['Leaf']
+    obj.update({"Result": result})
     return
 
 #recursive iterator of json data tree
-def recursive_iter(obj, cert, indent):
+def recursive_iter(obj, cert):
     if isinstance(obj, dict):
         for k, v in obj.items():
             if k == "Leaf":
-                compare_leaf(v, cert)
+                compare_leaf(obj, cert)
             else:
-                print(indent+'{', '"'+k+'": ', end="")
-                if k in cert:
-                    yield from recursive_iter(v, cert[k], indent+" ")
-                else:
-                    print('"'+ "WARNING -- the following key does not exist in provided certificate: " + k + '"', end="")
-                print(indent + '}')
+                yield from recursive_iter(v, cert)
+
     elif any(isinstance(obj, t) for t in (list, tuple)):
         first = 1
-        print(indent+'[')
         for item in obj:
             if first == 1:
                first = 0
             else:
                print(", ")
-            yield from recursive_iter(item, cert, indent+" ")
-        print(indent+']')
+            yield from recursive_iter(item, cert)
     yield obj
+
+def sigature_algorithm_func(self, pleaf):
+    cert_value = [self['signature_algorithm']['algorithm'].native]
+    if pleaf['Require'] == "2" and not( set(cert_value) <= set(pleaf['Value'] or cert_value == self['tbs_certificate']['signature']['algorithm'].native)):
+      result = "Failed with cert value: " + cert_value
+    else:
+      result ="PASS"
+    return  result
+
+def subject_func(self, pleaf):
+    cert_value = list(self.subject.native.keys())
+    if pleaf['Require'] == "2" and not( set(pleaf['Value']) <= set(cert_value) ):
+      result = "Failed with cert value: " + cert_value
+    else:
+      result ="PASS"
+    return  result
+
+def certificate_policies_func(self, pleaf):
+    cert_value = [self.certificate_policies_value.native[0]['policy_identifier']]
+    if pleaf['Require'] == "2" and not( set(cert_value) <= set(pleaf['Value'])) and not( set(['any_policy']) <= set(pleaf['Value'])):
+      result = "Failed with cert value: " + cert_value
+    else:
+      result ="PASS"
+    return  result
+
+conformance_check_functions = {
+    "sigature_algorithm_func": sigature_algorithm_func,
+    "subject_func": subject_func,
+    "certificate_policies_func": certificate_policies_func
+    }
 
 if __name__ == "__main__":
 
@@ -84,12 +117,14 @@ if __name__ == "__main__":
     with open('profiles/template.json') as json_data:
         json_profile = json.load(json_data)
 
-
+    json_profile["Basic Certificate Fields"]["Certificate Fields"]["signatureAlgorithm"]["Leaf"].update({"function": "sigature_algorithm_func"})
+    json_profile["Basic Certificate Fields"]["Certificate Fields"]["Subject"]["Leaf"].update({"function": "subject_func"})
+    json_profile["Certificate Extensions"]["Standard Extensions"]["Certificate Policies"]["Leaf"].update({"function": "certificate_policies_func"})
     try:
-        indent = "\n"
-        for item in recursive_iter(json_profile, input_cert, indent):
+        for item in recursive_iter(json_profile, input_cert):
             print(item)
     except:
         # todo add proper exception handlers
         print("Failed to parse the profile", sys.stderr)
+    json.dump(json_profile, sys.stdout, indent = 2)
  
