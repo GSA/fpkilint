c743425baaada02b3cbfb0ea0143384046eb1bf4
Chunde <godadada@users.noreply.github.com>
2017-10-13 09:07:58 -0400

    Update profile_comformance.py

--- src/profile_comformance.py
+++ src/profile_comformance.py
@@ -1,98 +1,283 @@
 from cert_helpers import *
 import json
-import sys
+import datetime
+import pytz
+import collections
+
+class config_entry:
+    def __init__(self):
+        self.value = ""
+        self.oid = ""
 
 
-#compare profile against cert leaf
-def compare_leaf(obj, cert):
-    profile_leaf = obj['Leaf']
-    result = conformance_check_functions[profile_leaf['function']](cert, profile_leaf)
-
-    """
-    if profile_leaf['Type'] == "OID":
-    elif profile_leaf['Type'] == "Binary":
-          cert_value = '{}'.format('%02X' % c for c in cert_value) 
-    elif profile_leaf['Type'] == "String":
-      	  cert_value = cert[profile_leaf.Name].native
-    elif profile_leaf['Type'] == "DateTime":
-      	  cert_value = cert[profile_leaf.Name]
-    elif profile_leaf['Type'] == "List":
-    elif profile_leaf['Type'] == "Bool":
-      	  cert_value = cert[profile_leaf.Name]
-    elif profile_leaf['Type'] == "Tuple":
-      	  cert_value = cert[profile_leaf.Name]
-    elif profile_leaf['Type'] == "OrderedDict":
-      	  cert_value = cert[profile_leaf.Name]
-    else:
-          print("\"WARNING: Unknow Leaf Type\"")
-          return
-   
-
-    if profile_leaf['Require'] == "0" and not (profile_leaf['Value'] == cert_value):
-      result = "WARNING: optional value is not valid"
-    elif profile_leaf['Require'] == "1" and profile_leaf['Value'] == cert_value:
-      result = "ERROR: value is not allowed - " + cert_value 
-    elif profile_leaf['Require'] == "2" and not(profile_leaf['Value'] == cert_value):
-      result = "ERROR: required value is not valid - "  + cert_value
-    elif profile_leaf['Require'] == "PROFILE_IN_CERT" and not( set(profile_leaf['Value']) <= set(cert_value) ):
-      result = "Failed with cert value: " + cert_value
-    elif profile_leaf['Require'] == "CERT_IN_PROFILE" and not(set(cert_value) <=set(profile_leaf['Value'])):
-      result = "Failed with cert value: " + cert_value 
-    else:
-      result = "PASS"
-     """
-    del obj['Leaf']
-    obj.update({"Result": result})
+def lint_other_extensions(cfg_options, cert, config_section):
+    #cert_leaf = cert. what is this?
+    #oid_match(cfg_options, cert_leaf, config_section, oid_list)
     return
 
-#recursive iterator of json data tree
-def recursive_iter(obj, cert):
-    if isinstance(obj, dict):
-        for k, v in obj.items():
-            if k == "Leaf":
-                compare_leaf(obj, cert)
-            else:
-                yield from recursive_iter(v, cert)
+def lint_policy_mappings(cfg_options, cert, config_section):
+    cert_leaf = cert.policy_mappings_value
+    oid_list = []
+    oid_match(cfg_options, cert_leaf, config_section, oid_list)
+    return
 
-    elif any(isinstance(obj, t) for t in (list, tuple)):
-        first = 1
-        for item in obj:
-            if first == 1:
-               first = 0
-            else:
-               print(", ")
-            yield from recursive_iter(item, cert)
-    yield obj
 
-def sigature_algorithm_func(self, pleaf):
-    cert_value = [self['signature_algorithm']['algorithm'].native]
-    if pleaf['Require'] == "2" and not( set(cert_value) <= set(pleaf['Value'] or cert_value == self['tbs_certificate']['signature']['algorithm'].native)):
-      result = "Failed with cert value: " + cert_value
+def lint_name_constraints(cfg_options, cert, config_section):
+    cert_leaf = cert.name_constraints_value
+    oid_list = []
+    oid_match(cfg_options, cert_leaf, config_section, oid_list)
+    return
+
+
+def lint_piv_naci(cfg_options, cert, config_section):
+    # missing in asn1
+    return
+
+
+def lint_validity(cfg_options, cert, config_section):
+    nb = cert['tbs_certificate']['validity']['not_before']
+    na = cert['tbs_certificate']['validity']['not_after']
+    lifespan = na.native - nb.native
+    #r.content += "Valid for {}".format(lifespan)
+
+    cut_off = datetime.datetime(2050, 1, 1)
+    cut_off = cut_off.replace(tzinfo=pytz.UTC)
+
+    if nb.name == 'utc_time':
+        if nb.native > cut_off:
+            print("notBefore is required to be GeneralizedTime")
+
+    return
+
+
+def lint_subject(cfg_options, cert, config_section):
+
+    if 'base_dn' in  cfg_options and len(cfg_options['base_dn']) > 0:
+        # todo: check for base_dn match if base_dn has value
+        print("fill in base dn check code")
+
+    cert_leaf = cert.subject
+    oid_list = []
+    rdn_seq = cert_leaf.chosen
+    for rdn in rdn_seq:
+         for name in rdn:
+              oid_list += [name['type'].dotted]
+    oid_match(cfg_options, cert_leaf, config_section, oid_list)
+
+    return
+
+
+def lint_key_usage(cfg_options, cert, config_section):
+    cert_leaf = cert.key_usage_value
+    oid_list = []
+    oid_match(cfg_options, cert_leaf, config_section, oid_list)
+    return
+
+
+def lint_issuer(cfg_options, cert, config_section):
+    cert_leaf = cert.issuer
+    oid_list = []
+    oid_match(cfg_options, cert_leaf, config_section, oid_list)
+    return
+
+
+def lint_akid(cfg_options, cert, config_section):
+    cert_leaf = cert.authority_key_identifier
+    oid_list = []
+    oid_match(cfg_options, cert_leaf, config_section, oid_list)
+    return
+
+def lint_skid(cfg_options, cert, config_section):
+    cert_leaf = cert['tbs_certificate']['subject_public_key_info']
+    oid_list = []
+    oid_match(cfg_options, cert_leaf, config_section, oid_list)
+    return
+
+def lint_policy_constraints(cfg_options, cert, config_section):
+    cert_leaf = cert.policy_constraints_value
+    oid_list = []
+    oid_match(cfg_options, cert_leaf, config_section, oid_list)
+    return
+
+
+def lint_serial_number(cfg_options, cert, config_section):
+    cert_leaf = cert.serial_number
+    oid_list = []
+    oid_match(cfg_options, cert_leaf, config_section, oid_list)
+    return
+
+
+def lint_basic_constraints(cfg_options, cert, config_section):
+    cert_leaf = cert.basic_constraints_value
+    oid_list = []
+    oid_match(cfg_options, cert_leaf, config_section, oid_list)
+    return
+
+
+def lint_cert_policies(cfg_options, cert, config_section):
+    cert_leaf = cert.certificate_policies_value
+    oid_list = [cert_leaf.native[0]['policy_identifier']]
+    oid_match(cfg_options, cert_leaf, config_section, oid_list)
+    return
+
+
+def lint_subject_public_key_info(cfg_options, cert, config_section):
+    cert_leaf = cert.public_key
+    oid_list = []
+    oid_match(cfg_options, cert_leaf, config_section, oid_list)
+    return
+
+
+def lint_aia(cfg_options, cert, config_section):
+    cert_leaf = cert.authority_information_access_value
+    oid_list = []
+    oid_match(cfg_options, cert_leaf, config_section, oid_list)
+    return
+
+
+def lint_san(cfg_options, cert, config_section):
+    cert_leaf = cert.subject_alt_name_value
+    oid_list = []
+    oid_match(cfg_options, cert_leaf, config_section, oid_list)
+    return
+
+
+def lint_ian(cfg_options, cert, config_section):
+    cert_leaf = cert.issuer_alt_name_value
+    oid_list = []
+    oid_match(cfg_options, cert_leaf, config_section, oid_list)
+    return
+
+
+def lint_eku(cfg_options, cert, config_section):
+    cert_leaf = cert.extended_key_usage_value
+    oid_list = []
+    oid_match(cfg_options, cert_leaf, config_section, oid_list)
+    return
+
+
+def lint_crldp(cfg_options, cert, config_section):
+    cert_leaf = cert.crl_distribution_points
+    oid_list = []
+    oid_match(cfg_options, cert_leaf, config_section, oid_list)
+    return
+
+
+def lint_sia(cfg_options, cert, config_section):
+    cert_leaf = cert.subject_information_access_value
+    oid_list = []
+    oid_match(cfg_options, cert_leaf, config_section, oid_list)
+    return
+
+
+def lint_pkup(cfg_options, cert, config_section):
+    #missing in asn1
+    return
+
+
+def lint_sub_dir_attr(cfg_options, cert, config_section):
+    cert_leaf = cert.subject_directory_attributes_value
+    oid_list = []
+    oid_match(cfg_options, cert_leaf, config_section, oid_list)
+    return
+
+
+def lint_signature_algorithm(cfg_options, cert, config_section):
+    cert_leaf = cert['signature_algorithm']['algorithm']
+    oid_list = [cert_leaf.dotted]
+    oid_match(cfg_options, cert_leaf, config_section, oid_list)
+    cert_leaf1 = cert['tbs_certificate']['signature']['algorithm'].dotted
+    if not cert_leaf.dotted == cert_leaf1:
+        print ("['signature_algorithm']['signature'] and ['tbs_certificate']['signature']['algorithm'] has differnt values: "
+            + str(cert_leaf.dotted) + " vs. " + str(cert_leaf1))
+    return
+
+
+def lint_version(cfg_options, cert, config_section):
+    cert_leaf = cert['tbs_certificate']['version']
+    oid_list = []
+    oid_match(cfg_options, cert_leaf, config_section, oid_list)
+    return
+
+
+def lint_ocsp_nocheck(cfg_options, cert, config_section):
+    cert_leaf = cert.ocsp_no_check_value
+    oid_list = []
+    oid_match(cfg_options, cert_leaf, config_section, oid_list)
+    return
+
+
+def lint_inhibit_any(cfg_options, cert, config_section):
+    cert_leaf = cert.inhibit_any_policy_value
+    oid_list = []
+    oid_match(cfg_options, cert_leaf, config_section, oid_list)
+    return
+
+def output(result, ce, cfg_opt, config_section, cert_value):
+    if result is True and cfg_opt[ce].value is '1' or result is False and cfg_opt[ce].value is '2':
+      print("FAIL with  Section: {}, Item: {}, Value: {}, OID: {} and Cert Value: {}"
+            .format(config_section, ce, cfg_opt[ce].value, cfg_opt[ce].oid, cert_value))
     else:
-      result ="PASS"
-    return  result
+      print("PASS with  Section: {}, Item: {}, Value: {}, OID: {} and Cert Value: {}"
+            .format(config_section, ce, cfg_opt[ce].value, cfg_opt[ce].oid, cert_value))
+    return
 
-def subject_func(self, pleaf):
-    cert_value = list(self.subject.native.keys())
-    if pleaf['Require'] == "2" and not( set(pleaf['Value']) <= set(cert_value) ):
-      result = "Failed with cert value: " + cert_value
+def oid_collect(cert_item, list):
+    if (hasattr(cert_item, 'chosen')):
+        cert_item = cert_item.chosen
+        for names in cert_item:
+            for name in names:
+                list = list + [name['type'].dotted]
     else:
-      result ="PASS"
-    return  result
+      if hasattr(cert_item, 'dotted'):
+        list = list + [cert_item.dotted]
+      elif (hasattr(cert_item, '__dict__') and isinstance(cert_item, collections.Iterable)):
+        for item in cert_item:
+            list = oid_collect(item, list)
 
-def certificate_policies_func(self, pleaf):
-    cert_value = [self.certificate_policies_value.native[0]['policy_identifier']]
-    if pleaf['Require'] == "2" and not( set(cert_value) <= set(pleaf['Value'])) and not( set(['any_policy']) <= set(pleaf['Value'])):
-      result = "Failed with cert value: " + cert_value
-    else:
-      result ="PASS"
-    return  result
+    return list
+
+def oid_match(cfg_options, cert_leaf, config_section, oid_list):
+    #oid_list = oid_collect(cert_item, [])
+    for ce in cfg_options:
+       result = False
+       for oid in oid_list:
+            if oid == cfg_options[ce].oid:
+                  result = True
+                  cert_value = oid
+       output(result, ce, cfg_options, config_section, oid_list)
+    return
 
 conformance_check_functions = {
-    "sigature_algorithm_func": sigature_algorithm_func,
-    "subject_func": subject_func,
-    "certificate_policies_func": certificate_policies_func
+    'Other Extensions': lint_other_extensions,
+    'Policy Mappings': lint_policy_mappings,
+    'Name Constraints': lint_name_constraints,
+    'PIV NACI': lint_piv_naci,
+    'Validity': lint_validity,
+    'Subject': lint_subject,
+    'Issuer': lint_issuer,
+    'Authority Key Identifier': lint_akid,
+    'Subject Key Identifier': lint_skid,
+    'Key Usage': lint_key_usage,
+    'Policy Constraints': lint_policy_constraints,
+    'Serial Number': lint_serial_number,
+    'Basic Constraints': lint_basic_constraints,
+    'Certificate Policies': lint_cert_policies,
+    'subjectPublicKeyInfo': lint_subject_public_key_info,
+    'Authority Information Access': lint_aia,
+    'Subject Alternative Name': lint_san,
+    'Issuer Alternative Name': lint_ian,
+    'Extended Key Usage': lint_eku,
+    'CRL Distribution Point': lint_crldp,
+    'Subject Information Access': lint_sia,
+    'Private Key Usage Period': lint_pkup,
+    'Subject Directory Attributes': lint_sub_dir_attr,
+    'Signature Algorithm': lint_signature_algorithm,
+    'Version': lint_version,
+    'OCSP No-Check': lint_ocsp_nocheck,
+    'Inhibit Any Policy': lint_inhibit_any
     }
+
 
 if __name__ == "__main__":
 
@@ -106,25 +291,31 @@ if __name__ == "__main__":
         input_cert = parse_cert(encoded)
     except:
         # todo add proper exception handlers
-        print("Failed to parse the certificate", sys.stderr)
+        print("Failed to parse the certificate")
 
     if input_cert is None:
         exit(0)
 
-    print("\nSubject:\n{}\n".format(get_pretty_dn(input_cert.subject, "\n", "=")), sys.stderr)
-    print("Issuer:\n{}\n".format(get_pretty_dn(input_cert.issuer, "\n", "=")), sys.stderr)
+    print("\nSubject:\n{}\n".format(get_pretty_dn(input_cert.subject, "\n", "=")))
+    print("Issuer:\n{}\n".format(get_pretty_dn(input_cert.issuer, "\n", "=")))
 
     with open('profiles/template.json') as json_data:
         json_profile = json.load(json_data)
 
-    json_profile["Basic Certificate Fields"]["Certificate Fields"]["signatureAlgorithm"]["Leaf"].update({"function": "sigature_algorithm_func"})
-    json_profile["Basic Certificate Fields"]["Certificate Fields"]["Subject"]["Leaf"].update({"function": "subject_func"})
-    json_profile["Certificate Extensions"]["Standard Extensions"]["Certificate Policies"]["Leaf"].update({"function": "certificate_policies_func"})
-    try:
-        for item in recursive_iter(json_profile, input_cert):
-            print(item)
-    except:
-        # todo add proper exception handlers
-        print("Failed to parse the profile", sys.stderr)
-    json.dump(json_profile, sys.stdout, indent = 2)
- 
+    cert_profile = {}
+
+    for entry in json_profile:
+        if entry['Section'] not in cert_profile:
+            cert_profile[entry['Section']] = {}
+        pce = config_entry()
+        pce.value = entry['Value']
+        pce.oid = entry['OID']
+        cert_profile[entry['Section']][entry['Item']] = pce
+
+    for config_section in cert_profile:
+        # print(config_section)
+        if config_section in conformance_check_functions:
+            conformance_check_functions[config_section](cert_profile[config_section], input_cert, config_section)
+        else:
+            print("Invalid config section:  {}".format(config_section))
+
