36907bebe042e1b19403d5986794c44455b0a004
Chunde <godadada@users.noreply.github.com>
2017-10-17 12:57:54 -0400

    Update profile_comformance.py

--- src/profile_comformance.py
+++ src/profile_comformance.py
@@ -3,6 +3,7 @@ import json
 import datetime
 import pytz
 import sys
+import ast
 
 class config_entry:
     def __init__(self):
@@ -124,7 +125,7 @@ def lint_subject(cfg_opt, cert, cfg_sect, outJson):
                     if name['type'].dotted == cfg_opt[ce].oid:
                         found = True
                         break
-            output(found, ce, cfg_opt, cfg_sect, str(name.native), outJson)
+            output(found, ce, cfg_opt, cfg_sect, name['type'].dotted, outJson)
         elif "subject_base_dn" in ce:
             for rdn in subject.native: # need oid for base_dn to search for oid
                 if 'base_dn' in rdn:
@@ -148,15 +149,15 @@ def lint_key_usage(cfg_opt, cert, cfg_sect, outJson):
     output(found, "key_usage_present", cfg_opt, cfg_sect, e['extn_id'].native, outJson)
     if found:
         output(e['critical'].native, "key_usage_is_critical", cfg_opt, cfg_sect, str(e['critical'].native), outJson)
-        output(e['extn_value'].native == 'digital_signature', "key_usage_digitalSignature", cfg_opt, cfg_sect, e['extn_value'].native, outJson)
-        output(e['extn_value'].native == 'non_repudiation', "key_usage_nonRepudiation", cfg_opt, cfg_sect, e['extn_value'].native, outJson)
-        output(e['extn_value'].native == 'key_encipherment', "key_usage_keyEncipherment", cfg_opt, cfg_sect, e['extn_value'].native, outJson)
-        output(e['extn_value'].native == 'data_encipherment', "key_usage_dataEncipherment", cfg_opt, cfg_sect, e['extn_value'].native, outJson)
-        output(e['extn_value'].native == 'key_agreement', "key_usage_keyAgreement", cfg_opt, cfg_sect,e['extn_value'].native, outJson)
-        output(e['extn_value'].native == 'key_cert_sign', "key_usage_keyCertSign", cfg_opt, cfg_sect, e['extn_value'].native, outJson)
-        output(e['extn_value'].native == 'crl_sign', "key_usage_cRLSign", cfg_opt, cfg_sect, e['extn_value'].native, outJson)
-        output(e['extn_value'].native == 'encipher_only', "key_usage_encipherOnly", cfg_opt, cfg_sect, e['extn_value'].native, outJson)
-        output(e['extn_value'].native == 'decipher_only', "key_usage_decipherOnly", cfg_opt, cfg_sect, e['extn_value'].native, outJson)
+        output('digital_signature' in e['extn_value'].native, "key_usage_digitalSignature", cfg_opt, cfg_sect, str(e['extn_value'].native), outJson)
+        output('non_repudiation' in e['extn_value'].native, "key_usage_nonRepudiation", cfg_opt, cfg_sect, str(e['extn_value'].native), outJson)
+        output('key_encipherment' in e['extn_value'].native, "key_usage_keyEncipherment", cfg_opt, cfg_sect, str(e['extn_value'].native), outJson)
+        output('data_encipherment' in e['extn_value'].native, "key_usage_dataEncipherment", cfg_opt, cfg_sect, str(e['extn_value'].native), outJson)
+        output('key_agreement' in e['extn_value'].native, "key_usage_keyAgreement", cfg_opt, cfg_sect,e['extn_value'].native, outJson)
+        output('key_cert_sign' in e['extn_value'].native, "key_usage_keyCertSign", cfg_opt, cfg_sect, str(e['extn_value'].native), outJson)
+        output('crl_sign' in e['extn_value'].native, "key_usage_cRLSign", cfg_opt, cfg_sect, str(e['extn_value'].native), outJson)
+        output('encipher_only' in e['extn_value'].native, "key_usage_encipherOnly", cfg_opt, cfg_sect, str(e['extn_value'].native), outJson)
+        output('decipher_only' in e['extn_value'].native, "key_usage_decipherOnly", cfg_opt, cfg_sect, str(e['extn_value'].native), outJson)
     else:
         missing("key_usage_is_critical", cfg_opt, cfg_sect, outJson)
         missing("key_usage_digitalSignature", cfg_opt, cfg_sect, outJson)
@@ -185,13 +186,13 @@ def lint_issuer(cfg_opt, cert, cfg_sect, outJson):
                     if name['type'].dotted == cfg_opt[ce].oid:
                         found = True
                         break
-            output(found, ce, cfg_opt, cfg_sect, str(rdn), outJson)
+            output(found, ce, cfg_opt, cfg_sect, name['type'].dotted, outJson)
         elif "subject_base_dn" in ce:
             for rdn in cert_leaf.native: # need oid for base_dn to search for oid
                 if 'base_dn' in rdn:
                     found_base_dn = True
                     break
-            output(found_base_dn, ce, cfg_opt, cfg_sect, str(rdn), outJson)
+            output(found_base_dn, ce, cfg_opt, cfg_sect, "base_dn missing", outJson)
     return
 
 
@@ -226,7 +227,7 @@ def lint_skid(cfg_opt, cert, cfg_sect, outJson):
                 found = True
                 break
 
-    output(found, "skid_present", cfg_opt, cfg_sect, e['extn_id'], outJson)
+    output(found, "skid_present", cfg_opt, cfg_sect, e['extn_id'].native, outJson)
     if found:
         output(e['critical'].native, "skid_is_critical", cfg_opt, cfg_sect, str(e['critical'].native), outJson)
         #TODO: output(e['require_method_one'], "skid_require_method_one", cfg_opt, cfg_sect, str(e['critical'].native), outJson)
@@ -260,8 +261,8 @@ def lint_policy_constraints(cfg_opt, cert, cfg_sect, outJson):
 def lint_serial_number(cfg_opt, cert, cfg_sect, outJson):
     cert_leaf = cert['tbs_certificate']['serial_number'].native
     ln = len(str(cert_leaf))
-    output(ln > int(cfg_opt["serial_min_length"].value),  "serial_min_length", cfg_opt, cfg_sect, cert_leaf, outJson)
-    output(ln < int(cfg_opt["serial_max_length"].value), "serial_max_length", cfg_opt, cfg_sect, cert_leaf, outJson)
+    output(ln > int(cfg_opt["serial_min_length"].value),  "serial_min_length", cfg_opt, cfg_sect, str(cert_leaf), outJson)
+    output(ln < int(cfg_opt["serial_max_length"].value), "serial_max_length", cfg_opt, cfg_sect, str(cert_leaf), outJson)
     return
 
 
@@ -299,7 +300,7 @@ def lint_cert_policies(cfg_opt, cert, cfg_sect, outJson):
 
     for ce in cfg_opt:
         if ce == "certificate_policies_present":
-            output(not c_policies == None, ce, cfg_opt, cfg_sect, str(e), outJson)
+            output(not c_policies == None, ce, cfg_opt, cfg_sect, e['extn_id'], outJson)
         elif ce == "certificate_policies_is_critical":
             if not c_policies == None:
                output(c_policies['critical'] == True, ce, cfg_opt, cfg_sect, str(c_policies['critical']), outJson)
@@ -355,8 +356,8 @@ def lint_aia(cfg_opt, cert, cfg_sect, outJson):
                 ca_issuers_present = True #search oid?
             elif item['access_method'] == 'ocsp':
                 ocsp_present = True
-        output(ca_issuers_present, "aia_ca_issuers_present", cfg_opt, cfg_sect, e['extn_value'].native, outJson)
-        output(ocsp_present, "aia_ocsp_present", cfg_opt, cfg_sect, e['extn_value'].native, outJson)
+        output(ca_issuers_present, "aia_ca_issuers_present", cfg_opt, cfg_sect, item['access_method'], outJson)
+        output(ocsp_present, "aia_ocsp_present", cfg_opt, cfg_sect, item['access_method'], outJson)
         #todo: requirements for other field
 
     else:
@@ -462,24 +463,24 @@ def lint_eku(cfg_opt, cert, cfg_sect, outJson):
     output(found, "eku_present", cfg_opt, cfg_sect, e['extn_id'].native, outJson)
     if found:
         output(e['critical'].native, "eku_is_critical", cfg_opt, cfg_sect, str(e['critical'].native), outJson)
-        output(e['extn_value'].native == cfg_opt['eku_oid_server_auth'].oid, "eku_oid_server_auth", cfg_opt, cfg_sect, e['extn_value'].native, outJson)
-        output(e['extn_value'].native == cfg_opt['eku_oid_client_auth'].oid, "eku_oid_client_auth", cfg_opt, cfg_sect, e['extn_value'].native, outJson)
-        output(e['extn_value'].native == cfg_opt['eku_oid_code_signing'].oid, "eku_oid_code_signing", cfg_opt, cfg_sect, e['extn_value'].native, outJson)
-        output(e['extn_value'].native == cfg_opt['eku_oid_email_protection'].oid, "eku_oid_email_protection", cfg_opt, cfg_sect, e['extn_value'].native, outJson)
-        output(e['extn_value'].native == cfg_opt['eku_oid_time_stamping'].oid, "eku_oid_time_stamping", cfg_opt, cfg_sect,e['extn_value'].native, outJson)
-        output(e['extn_value'].native == cfg_opt['eku_oid_ocsp_signing'].oid, "eku_oid_ocsp_signing", cfg_opt, cfg_sect, e['extn_value'].native, outJson)
-        output(e['extn_value'].native == cfg_opt['eku_oid_any_eku'].oid, "eku_oid_any_eku", cfg_opt, cfg_sect, e['extn_value'].native, outJson)
-        output(e['extn_value'].native == cfg_opt['eku_oid_smart_card_logon'].oid, "eku_oid_smart_card_logon", cfg_opt, cfg_sect, e['extn_value'].native, outJson)
-        output(e['extn_value'].native == cfg_opt['eku_oid_ipsec_ike_intermediate'].oid, "eku_oid_ipsec_ike_intermediate", cfg_opt, cfg_sect, e['extn_value'].native, outJson)
-        output(e['extn_value'].native == cfg_opt['eku_oid_ipsec_end_system'].oid, "eku_oid_ipsec_end_system", cfg_opt, cfg_sect, e['extn_value'].native, outJson)
-        output(e['extn_value'].native == cfg_opt['eku_oid_ipsec_tunnel_termination'].oid, "eku_oid_ipsec_tunnel_termination", cfg_opt, cfg_sect, e['extn_value'].native, outJson)
-        output(e['extn_value'].native == cfg_opt['eku_oid_ipsec_user'].oid, "eku_oid_ipsec_user", cfg_opt, cfg_sect, e['extn_value'].native, outJson)
-        output(e['extn_value'].native == cfg_opt['eku_oid_piv_card_auth'].oid, "eku_oid_piv_card_auth", cfg_opt, cfg_sect, e['extn_value'].native, outJson)
-        output(e['extn_value'].native == cfg_opt['eku_oid_pivi_content_signing'].oid, "eku_oid_pivi_content_signing", cfg_opt, cfg_sect, e['extn_value'].native, outJson)
-        output(e['extn_value'].native == cfg_opt['eku_oid_smartCardLogon'].oid, "eku_oid_smartCardLogon", cfg_opt, cfg_sect, e['extn_value'].native, outJson)
-        output(e['extn_value'].native == cfg_opt['eku_oid_pkinit_KPKdc'].oid, "eku_oid_pkinit_KPKdc", cfg_opt, cfg_sect, e['extn_value'].native, outJson)
-        output(e['extn_value'].native == cfg_opt['eku_oid_pkinit_KPClientAuth'].oid, "eku_oid_pkinit_KPClientAuth", cfg_opt, cfg_sect, e['extn_value'].native, outJson)
-        output(e['extn_value'].native == cfg_opt['eku_other'].oid, "eku_other", cfg_opt, cfg_sect, e['extn_value'].native, outJson)
+        output(e['extn_value'].native[0] == cfg_opt['eku_oid_server_auth'].oid, "eku_oid_server_auth", cfg_opt, cfg_sect, e['extn_value'].native[0], outJson)
+        output(e['extn_value'].native[0] == cfg_opt['eku_oid_client_auth'].oid, "eku_oid_client_auth", cfg_opt, cfg_sect, e['extn_value'].native[0], outJson)
+        output(e['extn_value'].native[0] == cfg_opt['eku_oid_code_signing'].oid, "eku_oid_code_signing", cfg_opt, cfg_sect, e['extn_value'].native[0], outJson)
+        output(e['extn_value'].native[0] == cfg_opt['eku_oid_email_protection'].oid, "eku_oid_email_protection", cfg_opt, cfg_sect, e['extn_value'].native[0], outJson)
+        output(e['extn_value'].native[0] == cfg_opt['eku_oid_time_stamping'].oid, "eku_oid_time_stamping", cfg_opt, cfg_sect,e['extn_value'].native[0], outJson)
+        output(e['extn_value'].native[0] == cfg_opt['eku_oid_ocsp_signing'].oid, "eku_oid_ocsp_signing", cfg_opt, cfg_sect, e['extn_value'].native[0], outJson)
+        output(e['extn_value'].native[0] == cfg_opt['eku_oid_any_eku'].oid, "eku_oid_any_eku", cfg_opt, cfg_sect, e['extn_value'].native[0], outJson)
+        output(e['extn_value'].native[0] == cfg_opt['eku_oid_smart_card_logon'].oid, "eku_oid_smart_card_logon", cfg_opt, cfg_sect, e['extn_value'].native[0], outJson)
+        output(e['extn_value'].native[0] == cfg_opt['eku_oid_ipsec_ike_intermediate'].oid, "eku_oid_ipsec_ike_intermediate", cfg_opt, cfg_sect, e['extn_value'].native[0], outJson)
+        output(e['extn_value'].native[0] == cfg_opt['eku_oid_ipsec_end_system'].oid, "eku_oid_ipsec_end_system", cfg_opt, cfg_sect, e['extn_value'].native[0], outJson)
+        output(e['extn_value'].native[0] == cfg_opt['eku_oid_ipsec_tunnel_termination'].oid, "eku_oid_ipsec_tunnel_termination", cfg_opt, cfg_sect, e['extn_value'].native[0], outJson)
+        output(e['extn_value'].native[0] == cfg_opt['eku_oid_ipsec_user'].oid, "eku_oid_ipsec_user", cfg_opt, cfg_sect, e['extn_value'].native[0], outJson)
+        output(e['extn_value'].native[0] == cfg_opt['eku_oid_piv_card_auth'].oid, "eku_oid_piv_card_auth", cfg_opt, cfg_sect, e['extn_value'].native[0], outJson)
+        output(e['extn_value'].native[0] == cfg_opt['eku_oid_pivi_content_signing'].oid, "eku_oid_pivi_content_signing", cfg_opt, cfg_sect, e['extn_value'].native[0], outJson)
+        output(e['extn_value'].native[0] == cfg_opt['eku_oid_smartCardLogon'].oid, "eku_oid_smartCardLogon", cfg_opt, cfg_sect, e['extn_value'].native[0], outJson)
+        output(e['extn_value'].native[0] == cfg_opt['eku_oid_pkinit_KPKdc'].oid, "eku_oid_pkinit_KPKdc", cfg_opt, cfg_sect, e['extn_value'].native[0], outJson)
+        output(e['extn_value'].native[0] == cfg_opt['eku_oid_pkinit_KPClientAuth'].oid, "eku_oid_pkinit_KPClientAuth", cfg_opt, cfg_sect, e['extn_value'].native[0], outJson)
+        output(e['extn_value'].native[0] == cfg_opt['eku_other'].oid, "eku_other", cfg_opt, cfg_sect, e['extn_value'].native[0], outJson)
 
     else:
         missing("eku_is_critical", cfg_opt, cfg_sect, outJson)
@@ -524,8 +525,8 @@ def lint_crldp(cfg_opt, cert, cfg_sect, outJson):
                 crldp_http = True #search oid?
             elif "ldap" in item['distribution_point']:
                 crldp_ldap = True
-        output(crldp_http, "crldp_http", cfg_opt, cfg_sect, e['extn_value'].native, outJson)
-        output(crldp_ldap, "crldp_ldap", cfg_opt, cfg_sect, e['extn_value'].native, outJson)
+        output(crldp_http, "crldp_http", cfg_opt, cfg_sect, str(item['distribution_point'][0]), outJson)
+        output(crldp_ldap, "crldp_ldap", cfg_opt, cfg_sect, str(item['distribution_point'][0]), outJson)
         #todo: requirements for other fields
 
     else:
@@ -607,7 +608,7 @@ def lint_signature_algorithm(cfg_opt, cert, cfg_sect, outJson):
     if not cert_leaf.dotted == cert_leaf1:
         print ("['signature_algorithm']['signature'] and ['tbs_certificate']['signature']['algorithm'] has differnt values: "
             + str(cert_leaf.dotted) + " vs. " + str(cert_leaf1))
-    output(found and cert_leaf.dotted == cert_leaf1, ce, cfg_opt, cfg_sect, str(cert_leaf.dotted), outJson)
+    output(found and cert_leaf.dotted == cert_leaf1, ce, cfg_opt, cfg_sect, cert_leaf.dotted, outJson)
     return
 
 
@@ -657,24 +658,26 @@ def lint_inhibit_any(cfg_opt, cert, cfg_sect, outJson):
 
 def output(result, ce, cfg_opt, cfg_sect, cert_value, outJson):
     if result is True and cfg_opt[ce].value is '1' or result is False and cfg_opt[ce].value is '2':
-      outJson.append("{'Section': '" + cfg_sect + "'," +
-                      "'Item': '" + ce + "'," +
-                      "'Value': '" + cfg_opt[ce].value + "'," +
-                      "'OID': '" + cfg_opt[ce].oid + "'," +
-                      "'OUTPUT': 'FAIL with Cert Value: " + str(cert_value) + "'}")
+      dictn = ast.literal_eval('{"Section": "' + cfg_sect + '",' +
+                      '"Item": "' + ce + '",' +
+                      '"Value": "' + cfg_opt[ce].value + '",' +
+                      '"OID": "' + cfg_opt[ce].oid + '",' +
+                      '"OUTPUT": "FAIL with Cert Value: ' + str(cert_value) + '"}')
     else:
-      outJson.append("{'Section': '" + cfg_sect + "'," +
-                      "'Item': '" + ce + "'," +
-                      "'Value': '" + cfg_opt[ce].value + "'," +
-                      "'OID': '" + cfg_opt[ce].oid + "'," +
-                      "'OUTPUT': 'PASS with Cert Value: " + str(cert_value) + "'}")
+      dictn = ast.literal_eval('{"Section": "' + cfg_sect + '",' +
+                      '"Item": "' + ce + '",' +
+                      '"Value": "' + cfg_opt[ce].value + '",' +
+                      '"OID": "' + cfg_opt[ce].oid + '",' +
+                      '"OUTPUT": "PASS with Cert Value: ' + str(cert_value) + '"}')
+    outJson.append(dictn.copy())
     return
 def missing(ce, cfg_opt, cfg_sect, outJson):
-    outJson.append("{'Section': '" + cfg_sect + "'," +
-                   "'Item': '" + ce + "'," +
-                   "'Value': '" + cfg_opt[ce].value + "'," +
-                   "'OID': '" + cfg_opt[ce].oid + "'," +
-                   "'OUTPUT': 'MISSING Cert Value'}")
+    dictn = ast.literal_eval('{"Section": "' + cfg_sect + '",' +
+                   '"Item": "' + ce + '",' +
+                   '"Value": "' + cfg_opt[ce].value + '",' +
+                   '"OID": "' + cfg_opt[ce].oid + '",' +
+                   '"OUTPUT": "MISSING Cert Value"}')
+    outJson.append(dictn.copy())
     return
 
 conformance_check_functions = {
